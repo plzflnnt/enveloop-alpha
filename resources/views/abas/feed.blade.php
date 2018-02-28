d3c803491580e72ec43b195cbff13cfa127e92e9
plzflnnt <pzneto@hotmail.com>
2018-02-27 22:26:59 -0300

    0.1

--- resources/views/abas/feed.blade.php
+++ resources/views/abas/feed.blade.php
@@ -1,17 +1,19 @@
-<div class="row">
-    <div class="col">
-
-        <div class="envelope-group">
-            <div class="row text-center">
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-                <div class="col-xs-4">Saldo x: R$ 300,00</div>
-            </div>
+<div class="envelope-group">
+    &nbsp;
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-outline-primary btn-sm">Adicionar envelope</button>
         </div>
-
+    </div>
+    &nbsp;
+    <div class="row">
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
+        <div class="col-sm-4">Saldo x: R$ 300,00</div>
     </div>
 </div>
+
